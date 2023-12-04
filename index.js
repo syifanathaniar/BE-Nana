e259ee74be7201dad0c74c7d085e0654a5afbcec
syifanathaniar <140411045+syifanathaniar@users.noreply.github.com>
2023-12-04 11:02:43 +0700

    update database all catalog and catalog by id

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { prisma } = require("./config/prisma");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
 
 app.get("/", async(req, res) => {
     res.send("here is the response");
@@ -13,10 +16,24 @@ app.get("/", async(req, res) => {
 
 
 // catalog Routes
+
+//get all catalog
 app.get("/catalogs", async(req, res) => {
     const catalog = await prisma.catalog.findMany();
     res.status(200).send(catalog);
-})
+});
+
+// get catalog by id
+app.get("/catalogs/:id", async(req, res) => {
+    const catalog = await prisma.catalog.findUnique({
+        where: {
+            id: parseInt(req.params.id),
+        },
+    });
+    if (!catalog) res.status(404).send("Catalog not Found");
+    else res.status(200).send(catalog);
+});
+
 // end  catalog routes
 // product Routes
 // end product routes
